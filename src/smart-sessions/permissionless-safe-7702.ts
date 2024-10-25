0417f5da8c59073ddb2c8252325e0ccce3b62bb3
kopy-kat <kopp.konrad@gmail.com>
2024-10-25 11:18:23 +0700

    feat: add smart sessions 7702 demo

--- src/smart-sessions/permissionless-safe-7702.ts
+++ src/smart-sessions/permissionless-safe-7702.ts
@@ -3,17 +3,17 @@ import {
   OWNABLE_VALIDATOR_ADDRESS,
   getSudoPolicy,
   Session,
-  getClient,
   getAccount,
   encodeSmartSessionSignature,
   getOwnableValidatorMockSignature,
   RHINESTONE_ATTESTER_ADDRESS,
   MOCK_ATTESTER_ADDRESS,
-  getTrustAttestersAction,
   encodeValidatorNonce,
   getOwnableValidator,
   encodeValidationData,
   getEnableSessionDetails,
+  SmartSessionMode,
+  getPermissionId,
 } from "@rhinestone/module-sdk";
 import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
 import {
@@ -24,6 +24,9 @@ import {
   http,
   Chain,
   toBytes,
+  parseAbi,
+  encodeFunctionData,
+  zeroAddress,
 } from "viem";
 import { createSmartAccountClient } from "permissionless";
 import { erc7579Actions } from "permissionless/actions/erc7579";
@@ -35,6 +38,8 @@ import {
 } from "viem/account-abstraction";
 import { toSafeSmartAccount } from "permissionless/accounts";
 import { getAccountNonce } from "permissionless/actions";
+import { signAuthorization } from "viem/experimental";
+import { writeContract } from "viem/actions";
 
 export default async function main({
   bundlerUrl,
@@ -64,79 +69,17 @@ export default async function main({
     transport: http(paymasterUrl),
   });
 
+  const sponsorAccount = privateKeyToAccount(
+    process.env.SPONSOR_PRIVATE_KEY! as Hex,
+  );
+
   const owner = privateKeyToAccount(generatePrivateKey());
+  // note: safe currently allows address(this) to be an owner
+  const safeOwner = privateKeyToAccount(generatePrivateKey());
 
   const ownableValidator = getOwnableValidator({
     owners: [owner.address],
     threshold: 1,
-  });
-
-  const safeAccount = await toSafeSmartAccount({
-    client: publicClient,
-    owners: [owner],
-    version: "1.4.1",
-    entryPoint: {
-      address: entryPoint07Address,
-      version: "0.7",
-    },
-    // safe4337ModuleAddress: "0x7579F9feedf32331C645828139aFF78d517d0001",
-    // erc7579LaunchpadAddress: "0x7579011aB74c46090561ea277Ba79D510c6C00ff",
-    safe4337ModuleAddress: "0x3Fdb5BC686e861480ef99A6E3FaAe03c0b9F32e2", // These are not meant to be used in production as of now.
-    erc7579LaunchpadAddress: "0xEBe001b3D534B9B6E2500FB78E67a1A137f561CE",
-    attesters: [
-      RHINESTONE_ATTESTER_ADDRESS, // Rhinestone Attester
-      MOCK_ATTESTER_ADDRESS, // Mock Attester - do not use in production
-    ],
-    attestersThreshold: 1,
-    validators: [
-      {
-        address: ownableValidator.address,
-        context: ownableValidator.initData,
-      },
-    ],
-  });
-
-  const smartAccountClient = createSmartAccountClient({
-    account: safeAccount,
-    chain: chain,
-    bundlerTransport: http(bundlerUrl),
-    paymaster: paymasterClient,
-    userOperation: {
-      estimateFeesPerGas: async () => {
-        return (await pimlicoClient.getUserOperationGasPrice()).fast;
-      },
-    },
-  }).extend(erc7579Actions());
-
-  const trustAttestersAction = getTrustAttestersAction({
-    threshold: 1,
-    attesters: [
-      RHINESTONE_ATTESTER_ADDRESS, // Rhinestone Attester
-      MOCK_ATTESTER_ADDRESS, // Mock Attester - do not use in production
-    ],
-  });
-
-  const userOpHash1 = await smartAccountClient.sendUserOperation({
-    account: safeAccount,
-    calls: [
-      {
-        to: trustAttestersAction.target,
-        value: BigInt(0),
-        data: trustAttestersAction.callData,
-      },
-    ],
-  });
-
-  await pimlicoClient.waitForUserOperationReceipt({
-    hash: userOpHash1,
-  });
-
-  const smartSessions = getSmartSessionsValidator({});
-
-  const opHash = await smartAccountClient.installModule(smartSessions);
-
-  await pimlicoClient.waitForUserOperationReceipt({
-    hash: opHash,
   });
 
   const sessionOwner = privateKeyToAccount(generatePrivateKey());
@@ -163,34 +106,111 @@ export default async function main({
     chainId: BigInt(chain.id),
   };
 
-  const account = getAccount({
-    address: safeAccount.address,
-    type: "safe",
-  });
-
-  const sessionDetails = await getEnableSessionDetails({
+  const smartSessions = getSmartSessionsValidator({
     sessions: [session],
-    account,
-    clients: [publicClient],
   });
 
-  sessionDetails.enableSessionData.enableSession.permissionEnableSig =
-    await owner.signMessage({
-      message: { raw: sessionDetails.permissionEnableHash },
-    });
+  const authorization = await signAuthorization(publicClient, {
+    account: owner,
+    contractAddress: "0x29fcB43b46531BcA003ddC8FCB67FFE91900C762",
+    delegate: sponsorAccount,
+  });
+
+  const txHash = await writeContract(publicClient, {
+    address: owner.address,
+    abi: parseAbi([
+      "function setup(address[] calldata _owners,uint256 _threshold,address to,bytes calldata data,address fallbackHandler,address paymentToken,uint256 payment, address paymentReceiver) external",
+    ]),
+    functionName: "setup",
+    args: [
+      [safeOwner.address],
+      BigInt(1),
+      "0x7579011aB74c46090561ea277Ba79D510c6C00ff",
+      encodeFunctionData({
+        abi: parseAbi([
+          "struct ModuleInit {address module;bytes initData;}",
+          "function addSafe7579(address safe7579,ModuleInit[] calldata validators,ModuleInit[] calldata executors,ModuleInit[] calldata fallbacks, ModuleInit[] calldata hooks,address[] calldata attesters,uint8 threshold) external",
+        ]),
+        functionName: "addSafe7579",
+        args: [
+          "0x7579EE8307284F293B1927136486880611F20002",
+          [
+            {
+              module: ownableValidator.address,
+              initData: ownableValidator.initData,
+            },
+            {
+              module: smartSessions.address,
+              initData: smartSessions.initData,
+            },
+          ],
+          [],
+          [],
+          [],
+          [
+            RHINESTONE_ATTESTER_ADDRESS, // Rhinestone Attester
+            MOCK_ATTESTER_ADDRESS, // Mock Attester - do not use in production
+          ],
+          1,
+        ],
+      }),
+      "0x7579EE8307284F293B1927136486880611F20002",
+      zeroAddress,
+      BigInt(0),
+      zeroAddress,
+    ],
+    account: sponsorAccount,
+    authorizationList: [authorization],
+  });
+
+  await publicClient.waitForTransactionReceipt({
+    hash: txHash,
+  });
+
+  const safeAccount = await toSafeSmartAccount({
+    address: owner.address,
+    client: publicClient,
+    owners: [safeOwner],
+    version: "1.4.1",
+    entryPoint: {
+      address: entryPoint07Address,
+      version: "0.7",
+    },
+    safe4337ModuleAddress: "0x7579EE8307284F293B1927136486880611F20002",
+    erc7579LaunchpadAddress: "0x7579011aB74c46090561ea277Ba79D510c6C00ff",
+  });
+
+  const smartAccountClient = createSmartAccountClient({
+    account: safeAccount,
+    chain: chain,
+    bundlerTransport: http(bundlerUrl),
+    paymaster: paymasterClient,
+    userOperation: {
+      estimateFeesPerGas: async () => {
+        return (await pimlicoClient.getUserOperationGasPrice()).fast;
+      },
+    },
+  }).extend(erc7579Actions());
 
   const nonce = await getAccountNonce(publicClient, {
     address: safeAccount.address,
     entryPointAddress: entryPoint07Address,
     key: encodeValidatorNonce({
-      account,
+      account: getAccount({
+        address: safeAccount.address,
+        type: "safe",
+      }),
       validator: smartSessions,
     }),
   });
 
-  sessionDetails.signature = getOwnableValidatorMockSignature({
-    threshold: 1,
-  });
+  const sessionDetails = {
+    mode: SmartSessionMode.USE,
+    permissionId: getPermissionId({ session }),
+    signature: getOwnableValidatorMockSignature({
+      threshold: 1,
+    }),
+  };
 
   const userOperation = await smartAccountClient.prepareUserOperation({
     account: safeAccount,
--- test/main.test.ts
+++ test/main.test.ts
@@ -1,6 +1,7 @@
-import { sepolia } from "viem/chains";
+import { odysseyTestnet, sepolia } from "viem/chains";
 import { ensureBundlerIsReady, ensurePaymasterIsReady } from "./healthCheck";
 import smartSessionsPermissionlessSafe from "../src/smart-sessions/permissionless-safe";
+import smartSessionsPermissionlessSafe7702 from "../src/smart-sessions/permissionless-safe-7702";
 import deadmanSwitchPermissionlessSafe from "../src/deadman-switch/permissionless-safe";
 import socialRecoveryPermissionlessSafe from "../src/social-recovery/permissionless-safe";
 import socialRecoveryZeroDevKernel from "../src/social-recovery/zerodev-kernel";
@@ -35,6 +36,17 @@ describe("Test erc7579 reference implementation", () => {
   //   expect(receipt.success).toBe(true);
   // }, 40000);
   //
+  it("should test smart sessions with permissionless and 7702", async () => {
+    const receipt = await smartSessionsPermissionlessSafe7702({
+      bundlerUrl,
+      rpcUrl,
+      paymasterUrl,
+      chain: sepolia,
+    });
+    expect(receipt.success).toBe(true);
+  }, 40000);
+
+  //
   // it("should test deadman switch with permissionless", async () => {
   //   const receipt = await deadmanSwitchPermissionlessSafe({
   //     bundlerUrl,
@@ -65,39 +77,39 @@ describe("Test erc7579 reference implementation", () => {
   //   expect(receipt.success).toBe(true);
   // }, 40000);
 
-  it("should test scheduled transfers with permissionless", async () => {
-    const logs = await scheduledTransfersPermissionlessSafe({
-      bundlerUrl: process.env.BUNDLER_URL!,
-      rpcUrl: process.env.RPC_URL!,
-      paymasterUrl: process.env.PAYMASTER_URL!,
-      chain: sepolia,
-      automationsApiKey: process.env.AUTOMATIONS_API_KEY!,
-    });
-    // expect(receipt.success).toBe(true);
-  }, 200000);
-
-  it("should test scheduled orders with permissionless", async () => {
-    const logs = await scheduledOrdersPermissionlessSafe({
-      bundlerUrl: process.env.BUNDLER_URL!,
-      rpcUrl: process.env.RPC_URL!,
-      paymasterUrl: process.env.PAYMASTER_URL!,
-      chain: sepolia,
-      automationsApiKey: process.env.AUTOMATIONS_API_KEY!,
-    });
-    // expect(receipt.success).toBe(true);
-  }, 200000);
-
-  it("should test auto savings with permissionless", async () => {
-    const logs = await autoSavingsPermissionlessSafePermissionlessSafe({
-      bundlerUrl: process.env.BUNDLER_URL!,
-      rpcUrl: process.env.RPC_URL!,
-      paymasterUrl: process.env.PAYMASTER_URL!,
-      chain: sepolia,
-      automationsApiKey: process.env.AUTOMATIONS_API_KEY!,
-    });
-    // expect(receipt.success).toBe(true);
-  }, 200000);
-
+  // it("should test scheduled transfers with permissionless", async () => {
+  //   const logs = await scheduledTransfersPermissionlessSafe({
+  //     bundlerUrl: `https://api.pimlico.io/v2/11155111/rpc?apikey=${process.env.PIMLICO_API_KEY}`,
+  //     rpcUrl: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+  //     paymasterUrl: `https://api.pimlico.io/v2/11155111/rpc?apikey=${process.env.PIMLICO_API_KEY}`,
+  //     chain: sepolia,
+  //     automationsApiKey: process.env.AUTOMATIONS_API_KEY!,
+  //   });
+  //   // expect(receipt.success).toBe(true);
+  // }, 200000);
+  //
+  // it("should test scheduled orders with permissionless", async () => {
+  //   const logs = await scheduledOrdersPermissionlessSafe({
+  //     bundlerUrl: `https://api.pimlico.io/v2/11155111/rpc?apikey=${process.env.PIMLICO_API_KEY}`,
+  //     rpcUrl: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+  //     paymasterUrl: `https://api.pimlico.io/v2/11155111/rpc?apikey=${process.env.PIMLICO_API_KEY}`,
+  //     chain: sepolia,
+  //     automationsApiKey: process.env.AUTOMATIONS_API_KEY!,
+  //   });
+  //   // expect(receipt.success).toBe(true);
+  // }, 200000);
+  //
+  // it("should test auto savings with permissionless", async () => {
+  //   const logs = await autoSavingsPermissionlessSafe({
+  //     bundlerUrl: `https://api.pimlico.io/v2/11155111/rpc?apikey=${process.env.PIMLICO_API_KEY}`,
+  //     rpcUrl: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+  //     paymasterUrl: `https://api.pimlico.io/v2/11155111/rpc?apikey=${process.env.PIMLICO_API_KEY}`,
+  //     chain: sepolia,
+  //     automationsApiKey: process.env.AUTOMATIONS_API_KEY!,
+  //   });
+  //   // expect(receipt.success).toBe(true);
+  // }, 200000);
+  //
   // todo: figure out how to run this in jest
   // it("should test webauhtn with permissionless", async () => {
   //   const receipt = await webauthnPermissionlessSafe({
